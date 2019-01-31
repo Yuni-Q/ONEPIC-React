6ec7fd8ebb5315a06f340faec0e6204ab5bccef7
Yuni-Q <carnotcycle2@naver.com>
2019-01-31 14:46:18 +0900

    chore: add temp

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,100 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import styled from 'styled-components';
-// import { Provider as ReduxProvider } from 'react-redux';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import styled from 'styled-components';
+import { connect} from 'react-redux';
+import { addAge } from 'src';
 
-const Home = () => {
-  const StyleHome = styled.div`
-    background-image: url('/Onepic_logo_main.png@3x.png');
-    background-color: black;
-    background-position: center; 
-    background-repeat: no-repeat;
-    background-size: 186px 59.3px;
-    position: fixed;
-    width 100%;
-    height: 100%;
-  `
+
+// class Home extends React.Component<HomeProps, HomeState> {
+
+//   constructor(props={}) {
+//     super(props);
+//     this.state = {
+//        redirect: false,
+//     };
+//   }
+  
+//   render() {
+//     const StyleHome = styled.div`
+//     background-image: url('/Onepic_logo_main.png@3x.png');
+//     background-color: black;
+//     background-position: center; 
+//     background-repeat: no-repeat;
+//     background-size: 186px 59.3px;
+//     position: fixed;
+//     width 100%;
+//     height: 100%;
+//     `
+    
+//     const redirect = this.state.redirect;
+//     return (
+//       <>
+//         <StyleHome onClick={() => {
+//             this.setState({ redirect: true })
+//         }} />
+//         {
+//           redirect ? <Redirect to="/boards" /> : null
+//         }
+//       </>
+//     );
+//   }
+// };
+
+// const boards = () => {
+//   return (
+//     <div>
+//       <h1>
+//         게시판 입니다.
+//       </h1>
+//     </div>
+//   );
+// };
+
+
+const App: React.SFC<AppProps> = (props) => {
   return (
-    <StyleHome />
+    <>
+      <h1>hi</h1>
+      {props.age}
+      <button onClick={props.onAddClick}>click</button>
+    </>
+    // <Router>
+    //   <>
+    //     <Route path="/" component={Home} />
+    //     <Route path="/boards" component={boards} />
+    //   </>
+    // </Router>
   );
 };
 
-const boards = () => {
-  return (
-    <div>
-      <h1>
-          게시판 입니다.
-      </h1>
-    </div>
-  );
-};
-
-
-
-
-
-
-class App extends React.Component {
-  public render() {
-    return (
-      <Router>
-        <>
-          <Route path="/" component={Home} />
-          <Route path="/boards" component={boards} />
-        </>
-      </Router>
-    );
+const mapStatetoProps = (state: {age: number;}) => {
+  return {
+     age: state.age
   }
 }
 
-export default App;
+const mapDispatchtoProps = (dispatch: Function) => {
+  return {
+    onAddClick: () => {
+      dispatch(addAge());
+    }
+  };
+};
+
+const AppContainer = connect(
+  mapStatetoProps,
+  mapDispatchtoProps
+)(App);
+
+export default AppContainer;
+
+// interface HomeProps {
+// }
+
+// interface HomeState {
+//   redirect: boolean;
+// }
+interface AppProps {
+  age: number;
+  onAddClick(): void;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,35 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
+import { Provider} from 'react-redux';
+
+import { createStore } from 'redux';
+
+
+const ADD_AGE = 'ADD_AGE';
+
+export function addAge() {
+  return {
+    type: ADD_AGE
+  }
+}
+
+function ageApp(state: { age: number; } = { age: 35 }, action: { type: 'ADD_AGE' }) {
+  if (action.type === ADD_AGE) {
+    return {
+      age: state.age + 1
+    }
+  }
+  return state;
+}
+
+const store = createStore(ageApp)
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
+// registerServiceWorker();
